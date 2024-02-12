073ba1ba7fda18c106a38e75906a28e366526051
RajkumarGapat <rajkumargapat345@gmail.com>
2024-02-12 22:14:51 +0530

    W3Schools

--- HashSetEx.java
+++ HashSetEx.java
@@ -13,6 +13,23 @@ public class HashSetEx
 	    cars.add("BMW");
 	    cars.add("Mazda");
 	    System.out.println(cars); // [Volvo, Mazda, Ford, BMW]
+	    
+	    
+	    // check if an item exists in a HashSet ----->
+	    System.out.println(cars.contains("Mazda")); // true ------> contains method
+	    System.out.println(cars.contains("dbhbf")); // false 
+	    System.out.println(cars.contains("45454")); // false
+	    
+	    // remove an item from HashSet ----->
+	    cars.remove("Volvo");
+	    System.out.println(cars);        // [Mazda, Ford, BMW]
 
+	    // get the size of HashSet ----->
+	    System.out.println(cars.size());   // 3 ---> size method
+	    
+	        
+	    // remove all items from a HashSet ----->
+	    cars.clear();            // clear method
+	    System.out.println(cars);  // []
 	}
 }
